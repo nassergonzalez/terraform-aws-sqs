bfe437e0a89deefd49afeb19d7741737874c7dee
Nasser Gonzalez Gonzalez <nasser.gonzalez@harness.io>
2025-07-07 14:29:46 +0100

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -54,56 +54,6 @@ module "unencrypted_sqs" {
   tags = local.tags
 }
 
-module "cmk_encrypted_sqs" {
-  source = "../../"
-
-  name            = "${local.name}-cmk"
-  use_name_prefix = true
-
-  kms_master_key_id                 = aws_kms_key.this.id
-  kms_data_key_reuse_period_seconds = 3600
-
-  # Dead letter queue
-  create_dlq = true
-  redrive_policy = {
-    # default is 5 for this module
-    maxReceiveCount = 10
-  }
-
-  tags = local.tags
-}
-
-module "sse_encrypted_sqs" {
-  source = "../../"
-
-  name                    = "${local.name}-sse"
-  sqs_managed_sse_enabled = true
-
-  # Dead letter queue
-  redrive_policy = {
-    deadLetterTargetArn = module.sse_encrypted_dlq_sqs.queue_arn
-    maxReceiveCount     = 10
-  }
-
-  tags = local.tags
-}
-
-module "sse_encrypted_dlq_sqs" {
-  source = "../../"
-
-  # This is a separate queue used as a dead letter queue for the above example
-  # instead of the module creating both the queue and dead letter queue together
-
-  name                    = "${local.name}-sse-dlq"
-  sqs_managed_sse_enabled = true
-
-  # Dead letter queue
-  dlq_redrive_allow_policy = {
-    sourceQueueArns = [module.sse_encrypted_sqs.queue_arn]
-  }
-
-  tags = local.tags
-}
 
 module "sqs_with_dlq" {
   source = "../../"
@@ -166,9 +116,3 @@ module "disabled_sqs" {
 
   create = false
 }
-
-################################################################################
-# Supporting resources
-################################################################################
-
-resource "aws_kms_key" "this" {}
